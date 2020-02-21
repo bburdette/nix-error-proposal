a521b2edbb2d59545fbb99eea6c98acf49063158
Ben Burdette <bburdette@gmail.com>
2020-02-21 15:57:13 -0700

    previous, next lines, some refactoring

--- colorerrors/src/main.rs
+++ colorerrors/src/main.rs
@@ -7,15 +7,21 @@ struct ErrInfo {
     err_name: String,
     tool_name: String,
     description: String,
+    nix_code: Option<NixCode>,
+    hint: ColoredString,
+}
+
+struct NixCode {
     nix_file: Option<String>,
     err_line: Option<ErrLine>,
-    hint: ColoredString,
 }
 
 struct ErrLine {
     line_no: i32,
     column_range: Option<(usize, usize)>,
+    prev_loc: Option<String>,
     loc: String,
+    next_loc: Option<String>,
 }
 
 enum ErrLevel {
@@ -28,6 +34,27 @@ fn show_err_line(eline: &ErrLine) -> String {
         Some((start, _end)) => format!("({}:{})", eline.line_no, start),
         None => format!("({})", eline.line_no),
     }
+}
+
+fn print_code_lines(prefix: &str, nix_code: &NixCode) {
+  match &nix_code.err_line {
+      Some(eline) => {
+          eline.prev_loc.as_ref().map(|loc| println!("{}{}: {}", prefix, eline.line_no - 1, loc));
+          println!("{}{}: {}", prefix, eline.line_no, eline.loc);
+          eline.column_range.map(
+            |(col,len)|
+            println!(
+                  "{}    {}{}",
+                  prefix,
+                  " ".to_string().repeat(col),
+                  "^".to_string().repeat(len).red()
+              ));
+          eline.next_loc.as_ref().map(|loc| println!("{}{}: {}", prefix, eline.line_no + 1, loc));
+
+          println!("");
+      }
+      None => (),
+  }
 }
 
 fn print_error(einfo: &ErrInfo) {
@@ -61,41 +88,34 @@ fn print_error(einfo: &ErrInfo) {
     );
 
     // filename
-    match (&einfo.nix_file, &einfo.err_line) {
-        (Some(fname), Some(eline)) => {
-            println!("{}in file: {} {}", prefix, fname.blue(), show_err_line(eline).blue());
-            println!("{}", prefix);
+    match &einfo.nix_code {
+      None => (),
+      Some(nix_code) => {
+        match (&nix_code.nix_file, &nix_code.err_line) {
+            (Some(fname), Some(eline)) => {
+                println!("{}in file: {} {}", prefix, fname.blue(), show_err_line(eline).blue());
+                println!("{}", prefix);
+            }
+            (Some(fname), None) => {
+                println!("{}in file: {}", prefix, fname.blue());
+                println!("{}", prefix);
+            }
+            (None, _) => {
+                println!("{}from command line argument", prefix);
+                println!("{}", prefix);
+            }
         }
-        (Some(fname), None) => {
-            println!("{}in file: {}", prefix, fname.blue());
-            println!("{}", prefix);
-        }
-        (None, _) => {
-            println!("{}from command line argument", prefix);
-            println!("{}", prefix);
-        }
+      }
     }
+
 
     // description
     println!("{}{}", prefix, einfo.description);
     println!("{}", prefix);
 
     // line of code
-    match &einfo.err_line {
-        Some(eline) => {
-            println!("{}{}: {}", prefix, eline.line_no, eline.loc);
-            match &eline.column_range {
-                Some((col, len)) => println!(
-                    "{}    {}{}",
-                    prefix,
-                    " ".to_string().repeat(*col),
-                    "^".to_string().repeat(*len).red()
-                ),
-                None => (),
-            }
-        }
-        None => (),
-    }
+    einfo.nix_code.as_ref().map(|nc| print_code_lines(prefix,nc ));
+
 
     // hint
     println!("{}{}", prefix, einfo.hint);
@@ -108,12 +128,15 @@ fn main() {
         err_name: "error name".to_string(),
         tool_name: "nix tool name".to_string(),
         description: "general error description".to_string(),
+        nix_code: Some(NixCode {
         nix_file: Some("myfile.nix".to_string()),
         err_line: Some(ErrLine {
             line_no: 7,
             column_range: Some((22, 14)),
             loc: "line of code where the error occurred".to_string(),
-        }),
+            prev_loc: Some("previous line".to_string()),
+            next_loc: Some("next line".to_string()),
+        })}),
         hint: format!(
             "error hint with templated {}",
             "values".to_string().yellow()
@@ -126,12 +149,15 @@ fn main() {
         err_name: "Attribute Name".to_string(),
         tool_name: "nix-build".to_string(),
         description: "Attribute format is incorrect.  Only letters a-z, A-Z, 0-9, or one of \"+_-\" are allowed.".to_string(),
+        nix_code: Some(NixCode {
         nix_file: None,
         err_line: Some(ErrLine {
             line_no: 1,
             column_range: Some((2,8)),
+            prev_loc: None,
+            next_loc: None,
             loc: "{ \"hi.there\" = (import <nixpkgs> {}).hello; }".to_string(),
-        }),
+        })}),
         hint: format!("The symbol {} doesn't satisfy attribute naming requirements.  It will be ignored.", "hi.there".yellow()).to_string().white(),
     };
 
@@ -140,12 +166,16 @@ fn main() {
         err_name: "String Error".to_string(),
         tool_name: "nix-build".to_string(),
         description: "Invalid escape character.  Only \\t \\n \\r \\\\ are allowed.".to_string(),
+        nix_code : Some(NixCode {
         nix_file: None,
         err_line: Some(ErrLine {
             line_no: 1,
             column_range: Some((13, 2)),
             loc: "{ foo = \"test \\e\"; }".to_string(),
-        }),
+            next_loc: None,
+            prev_loc: None,
+        })
+          }) ,
         hint: format!(
             "{} is an invalid escape character for a nix string.",
             "\\e".yellow()
@@ -159,12 +189,16 @@ fn main() {
         err_name: "fetchGit Error".to_string(),
         tool_name: "nix build".to_string(),
         description: "builtin.fetchGit returned an error.".to_string(),
+        nix_code : Some(NixCode {
         nix_file: Some("default.nix".to_string()),
         err_line: Some(ErrLine {
             line_no: 101,
             column_range: Some((1, 16)),
+            prev_loc: None,
             loc: "builtin.fetchGit {".to_string(),
-        }),
+            next_loc: Some("  url=https://github.com/nixos/nixpkgs-channels;".to_string()),
+          })
+          }),
         hint: "fetchGit takes 4 arguments in a nix expression:
       { url 
       , rev (optional)
@@ -184,8 +218,7 @@ fn main() {
         err_name: "user-only garbage collection".to_string(),
         tool_name: "nix-collect-garbage".to_string(),
         description: "collecting garbage for user account only.".to_string(),
-        nix_file: None,
-        err_line: None,
+        nix_code : None,
         hint: format!(
             "this command will collect garbage for user {} only.
   To remove OS generations, run as root or use sudo.  See the manual for more.
@@ -200,8 +233,7 @@ fn main() {
         err_name: "remote builder error".to_string(),
         tool_name: "nix-store".to_string(),
         description: "the remote builder doesn't meet package requirements.".to_string(),
-        nix_file: None,
-        err_line: None,
+        nix_code : None,
         hint: format!(
             "This build has requiredSystemFeatures: {}
   But the remote machine only has: {}.
